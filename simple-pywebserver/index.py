1fbfcb4e7e74688e02ed650209be9af72a6b0643
jibespanola <tokin21@outlook.com>
2021-05-29 02:48:29 +0800

    added Dynamic POST and GET endpoints

--- simple-pywebserver/index.py
+++ simple-pywebserver/index.py
@@ -1,11 +1,13 @@
 import tornado.web #request handlers that receive from the http protocl
 import tornado.ioloop #thread that continously waits for requests
+import json
+import os
 
 class basicRequestHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello World! This is a Python command executed from the backend.")
 
-class listRequestHandler(tornado.web.RequestHandler):
+class staticListRequestHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html") #to serve static files
 
@@ -24,16 +26,35 @@ class resourceParamRequestHandler(tornado.web.RequestHandler):
     def get(self, studentName, courseId):
         self.write(f"Welcome {studentName}! the course you are viewing is {courseId}")
 
+#GET endpoint that queries files and returns JSON arrays
+class listRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        print(f"Current working directory is {os.getcwd()}")
+        fh = open("list.txt", "r")
+        fruits = fh.read().splitlines() # read entire file and split all lines
+        fh.close()
+        self.write(json.dumps(fruits))
+
+    #POST endpoint that creates entrypoint to the backend
+    def post(self):
+        fruit = self.get_argument("fruit") #query parameter
+        print(f"Current working directory is {os.getcwd()}")
+        fh = open("list.txt", "a") #second arg: "a" append to file
+        fh.write(f"{fruit}\n")
+        fh.close()
+        self.write(json.dumps({"message": "Fruit added successfully."}))
+
 #This should only run once
 if __name__ == "__main__":
     #class Application expects tuples of endpoints
     app = tornado.web.Application([
         (r"/", basicRequestHandler),
-        (r"/languages", listRequestHandler),
+        (r"/languages", staticListRequestHandler),
         (r"/isEven", queryParamRequestHandler),
-        (r"/students/([a-z]+)/([0-9]+)", resourceParamRequestHandler) 
+        (r"/students/([a-z]+)/([0-9]+)", resourceParamRequestHandler),
         #regular expressions help capture certain patterns
         #regular expression above is case sensitive
+        (r"/list", listRequestHandler)
     ])
 
     port = 8882
